f62405c89be9b96636354b3d7ca9806928b428e5
IleanaEscobar <izescobar5@gmail.com>
2022-06-27 15:03:28 -0700

    Add and implement error classes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const giftExchange = require('./routes/gift-exchange')
+const Errors = require('./utils/errors')
 
 app.use(bodyParser.json());
 app.use(morgan('tiny'))
@@ -14,4 +15,21 @@ app.get('/', (req, res) => {
   res.status(200).send({ "ping": "pong" })
 })
 
+app.use((error, next) => {
+    next(new NotFoundError)
+})
+
+app.use((error, req, res, next) => {
+    let status = 0
+    let message = ''
+    (error.status > 0)? (status = error.status) : (status = 500)
+    (error.message.length > 0)? (message = error.message) : (message = "Something wen't wrong in the application")
+    res.send({
+    error: {
+        status: status,
+        message: message
+    }
+    })
+
+})
 module.exports = app;
